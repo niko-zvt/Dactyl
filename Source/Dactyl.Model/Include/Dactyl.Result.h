d452d240aa7f10f20e9436b08917083469034d3a
Nikolay Zhivotenko <niko.zvt@gmail.com>
2023-06-01 10:28:02 +0300

    Added model result enum

---
+++ Source/Dactyl.Model/Include/Dactyl.Result.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _Dactyl_Result_h_
+#define _Dactyl_Result_h_
+
+namespace Dactyl::Model
+{
+    enum Result
+    {
+        /// Successful call. No Errors
+        Success = 0,
+    };
+}
+
+#endif /* _Dactyl.Result.h_ */
