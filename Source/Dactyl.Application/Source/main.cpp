1d40fd3241fa7d7f5d82174c8d1830246a58b671
Nikolay Zhivotenko <niko.zvt@gmail.com>
2023-06-01 09:25:02 +0300

    Added The Service Locator Pattern
    - Added IModel interface;
    - Added Model Locator;
    - Added FE Model class;
    - Added simple model test;

--- Source/Dactyl.Application/Source/main.cpp
+++ Source/Dactyl.Application/Source/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include "Dactyl.Utils.h"
-#include "Dactyl.Model.h"
+#include "Dactyl.IModel.h"
+#include "Dactyl.ModelLocator.h"
+#include "Dactyl.FEModel.h"
 
 int main(int argc, char *argv[])
 {
-
+    // Parse args
     if(argc > 1)
     {
         Dactyl::Application::Utils::parse_args(argc, const_cast<const char**>(argv));
     }
 
-    hello_model();
+    // Create a unique FE model and provide it to the model locator
+    std::unique_ptr<Dactyl::Model::FEModel> feModel = std::make_unique<Dactyl::Model::FEModel>();
+    Dactyl::Model::ModelLocator::provideModel(feModel.get());
+
+    // Test model
+    Dactyl::Model::IModel& model = Dactyl::Model::ModelLocator::getModel();
+    model.print();
 
     return 0;
 }
---
+++ Source/Dactyl.Model/Source/Dactyl.FEModel.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Calc.h"
+#include "Dactyl.FEModel.h"
+#include <Core>
+#include <Dense>
+
+namespace Dactyl::Model
+{
+
+    void FEModel::hello_model()
+    {
+        auto suma = test_sum(5.1, 4.2);
+        std::cout << "Hello, Model!\n\t5.1 + 4.2 = 9.3 == " << suma << std::endl;
+
+        Eigen::Matrix3d test; //3 by 3 double precision matrix initialization
+
+        // Let's make it a symmetric matrix
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+                test(i,j) = (i+1)*(1+j);
+        }
+
+        print_matrix(test);
+    }
+
+    void FEModel::print_matrix(Eigen::MatrixX3d m)
+    {
+        std::cout << m << std::endl;
+    }
+}
